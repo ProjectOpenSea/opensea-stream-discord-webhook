eec89240904da41a7f772673102b69fbe19f7669
Michael Cohen <michael.cohen@opensea.io>
2022-05-03 11:16:38 -0700

    Newest type changes to SDK

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,14 @@
-import {EventType, ItemListedEventPayload, ItemCancelledEventPayload, ItemMetadataUpdatePayload, ItemSoldEventPayload, ItemTransferredEventPayload, ItemReceivedBidEventPayload, ItemReceivedOfferEventPayload} from '../local-dependencies/opensea-stream-js-sdk/src/index';
+import {EventType, BaseItemType, ItemListedEventPayload, ItemCancelledEventPayload, ItemMetadataUpdatePayload, ItemSoldEventPayload, ItemTransferredEventPayload, ItemReceivedBidEventPayload, ItemReceivedOfferEventPayload} from '../local-dependencies/opensea-stream-js-sdk/src/index';
 
-type Item = {
-	token_id: string;
-	contract_address: string;
+export function getTitle(collection: string, item: BaseItemType): string {
+  return `${collection} #${item.nft_id}`;
 }
 
-export function getTitle(collection: string, item: Item): string {
-  return `${collection} #${item.token_id}`;
+export function getUrl(item: BaseItemType): string {
+	return item.permalink
 }
 
-export function getUrl(item: Item): string {
-	return `https://opensea.io/assets/${item.contract_address}/${item.token_id}`;
-}
-
-export function getMessage(item: Item, collection: string, event_type: string, timestamp: string, payload: any): string {
+export function getMessage(item: BaseItemType, collection: string, event_type: string, timestamp: string, payload: any): string {
 	let message = `${collection} ${event_type}`;
 	switch (event_type) {
 		case EventType.ITEM_LISTED.valueOf():
@@ -53,40 +48,40 @@ function priceCalc(base_price: number, decimals: number, usd_price: string): str
 	return formatter.format((base_price / Math.pow(10, decimals)) * parseFloat(usd_price));
 }
 
-function buildMessageItemListed(item: Item, payload: ItemListedEventPayload): string {
+function buildMessageItemListed(item: BaseItemType, payload: ItemListedEventPayload): string {
 	const price = priceCalc(payload.base_price, payload.payment_token.decimals, payload.payment_token.usd_price);
 	const quantity_extension = payload.quantity > 1 ? ` for ${payload.quantity} items` : '';
-	return `Token #${item.token_id} listed for ${price} by Owner ${payload.maker.address} ${quantity_extension}`;
+	return `Token #${item.nft_id} listed for ${price} by Owner ${payload.maker.address} ${quantity_extension}`;
 }
 
-function buildMessageItemCancelled(item: Item, payload: ItemCancelledEventPayload): string {
-	return `Order for Token #${item.token_id} cancelled.`;	
+function buildMessageItemCancelled(item: BaseItemType, payload: ItemCancelledEventPayload): string {
+	return `Order for Token #${item.nft_id} cancelled.`;	
 }
 
-function buildMessageItemMetadataUpdated(item: Item, payload: ItemMetadataUpdatePayload): string {
-	return `Metadata updated for Token #${item.token_id}`
+function buildMessageItemMetadataUpdated(item: BaseItemType, payload: ItemMetadataUpdatePayload): string {
+	return `Metadata updated for Token #${item.nft_id}`
 }
 
-function buildMessageItemSold(item: Item, payload: ItemSoldEventPayload): string {
+function buildMessageItemSold(item: BaseItemType, payload: ItemSoldEventPayload): string {
 	const price = priceCalc(payload.sale_price, payload.payment_token.decimals, payload.payment_token.usd_price);
 	const quantity_extension = payload.quantity > 1 ? `${payload.quantity} items of` : '';
 	const private_extension = payload.is_private ? 'in a private sale' : '';
-	return `${quantity_extension} Token #${item.token_id} sold to ${payload.taker.address} for ${price} ${private_extension}`;
+	return `${quantity_extension} Token #${item.nft_id} sold to ${payload.taker.address} for ${price} ${private_extension}`;
 }
 
-function buildMessageItemTransferred(item: Item, payload: ItemTransferredEventPayload): string {
+function buildMessageItemTransferred(item: BaseItemType, payload: ItemTransferredEventPayload): string {
 	const quantity_extension = payload.quantity > 1 ? `${payload.quantity} items of` : '';
-	return `${quantity_extension} Token #${item.token_id} transferred from ${payload.from_account.address} to ${payload.to_account.address}`;
+	return `${quantity_extension} Token #${item.nft_id} transferred from ${payload.from_account.address} to ${payload.to_account.address}`;
 }
 
-function buildMessageItemReceivedOffer(item: Item, payload: ItemReceivedOfferEventPayload): string {
+function buildMessageItemReceivedOffer(item: BaseItemType, payload: ItemReceivedOfferEventPayload): string {
 	const price = priceCalc(payload.base_price, payload.payment_token.decimals, payload.payment_token.usd_price);
 	const quantity_extension = payload.quantity > 1 ? ` for ${payload.quantity} items` : '';
-	return `Token #${item.token_id} received an offer for ${price} from ${payload.maker.address} ${quantity_extension}`;
+	return `Token #${item.nft_id} received an offer for ${price} from ${payload.maker.address} ${quantity_extension}`;
 }
 
-function buildMessageItemReceivedBid(item: Item, payload: ItemReceivedBidEventPayload): string {
+function buildMessageItemReceivedBid(item: BaseItemType, payload: ItemReceivedBidEventPayload): string {
 	const price = priceCalc(payload.base_price, payload.payment_token.decimals, payload.payment_token.usd_price);
 	const quantity_extension = payload.quantity > 1 ? ` for ${payload.quantity} items` : '';
-	return `Token #${item.token_id} received a bid for ${price} from ${payload.maker.address} ${quantity_extension}`;
+	return `Token #${item.nft_id} received a bid for ${price} from ${payload.maker.address} ${quantity_extension}`;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { MessageEmbed, WebhookClient } from "discord.js";
 import * as express from 'express';
 import * as dotenv from "dotenv";
-import { OpenSeaStreamClient, EventType, LogLevel } from '../local-dependencies/opensea-stream-js-sdk/src/index';
+import { OpenSeaStreamClient, EventType, LogLevel, Network } from '../local-dependencies/opensea-stream-js-sdk/src/index';
 import { WebSocket } from 'ws';
 import { getMessage, getTitle, getUrl } from './helpers';
 
@@ -12,7 +12,6 @@ const enabled = process.env.BOT_ENABLED || true;
 const webhookId = process.env.WEBHOOK_ID || "";
 const webhookToken = process.env.WEBHOOK_TOKEN || "";
 const openseaApiToken = process.env.OPENSEA_API_TOKEN || "";
-const openseaApiUrl = process.env.OPENSEA_API_URL || "";
 
 // Discord Webhook Client
 const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
@@ -20,7 +19,7 @@ const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
 // OpenSea Stream API Client
 const openseaClient = new OpenSeaStreamClient({
     token: openseaApiToken,
-    apiUrl: openseaApiUrl,
+    network: Network.MAINNET,
     connectOptions: {
       transport: WebSocket
     },
@@ -37,26 +36,13 @@ app.get("/", (req: express.Request, res: express.Response) => {
 		return;
 	}
 	const allEvents = Object.values(EventType);
-	openseaClient.onEvents('doodles-official', allEvents, (event: any) => {
-		console.log(event);
-		const embed = new MessageEmbed()
-			.setTitle(getTitle("Doodles", event.item))
-			.setDescription(getMessage(event.item, "Doodles", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.item))
-			.setColor('#FE9FDD');
-		webhookClient.send({
-			username: 'DoodleBot', 
-			avatarURL: 'https://lh3.googleusercontent.com/7B0qai02OdHA8P_EOVK672qUliyjQdQDGNrACxs7WnTgZAkJa_wWURnIFKeOh5VTf8cfTqW3wQpozGedaC9mteKphEOtztls02RlWQ=s130',
-			embeds: [embed],
-		});
-	});
 
 	openseaClient.onEvents('aurory', allEvents, (event: any) => {
 		console.log(event);
 		const embed = new MessageEmbed()
-			.setTitle(getTitle("Aurory", event.item))
-			.setDescription(getMessage(event.item, "Neon District", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.item))
+			.setTitle(getTitle("Aurory", event.payload.item))
+			.setDescription(getMessage(event.payload.item, "Neon District", event.event_type, event.timestamp, event.payload))
+			.setURL(getUrl(event.payload.item))
 			.setColor('#61C7D7');
 		webhookClient.send({
 			username: 'AuroryBots',
@@ -68,9 +54,9 @@ app.get("/", (req: express.Request, res: express.Response) => {
 	openseaClient.onEvents('boredapeyachtclub', allEvents, (event: any) => {
 		console.log(event);
 		const embed = new MessageEmbed()
-			.setTitle(getTitle("Bored Ape Yacht Club", event.item))
-			.setDescription(getMessage(event.item, "Bored Ape Yacht Club", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.item))
+			.setTitle(getTitle("Bored Ape Yacht Club", event.payload.item))
+			.setDescription(getMessage(event.payload.item, "Bored Ape Yacht Club", event.event_type, event.timestamp, event.payload))
+			.setURL(getUrl(event.payload.item))
 			.setColor('#EF972C');
 		webhookClient.send({
 			username: 'BoredApeBot',
@@ -82,12 +68,12 @@ app.get("/", (req: express.Request, res: express.Response) => {
 	openseaClient.onEvents('decentraland-polygon-wearables', allEvents, (event: any) => {
 		console.log(event);
 		const embed = new MessageEmbed()
-			.setTitle(getTitle("decentraland-polygon-wearables", event.item))
-			.setDescription(getMessage(event.item, "decentraland-polygon-wearables", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.item))
+			.setTitle(getTitle("decentraland-polygon-wearables", event.payload.item))
+			.setDescription(getMessage(event.payload.item, "decentraland-polygon-wearables", event.event_type, event.timestamp, event.payload))
+			.setURL(getUrl(event.payload.item))
 			.setColor('#EF972C');
 		webhookClient.send({
-			username: 'BoredApeBot',
+			username: 'Decentraland',
 			avatarURL: 'https://lh3.googleusercontent.com/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB=s130',
 			embeds: [embed],
 		});
