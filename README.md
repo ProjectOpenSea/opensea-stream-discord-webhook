d166cfe8946a4900629e0af6777c918ccf5ffbfb
Michael Cohen <michael.cohen@opensea.io>
2022-05-05 13:18:38 -0700

    First steps towards a more prod-ready ape-bot

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # opensea-stream-discord-webhook
+
 A sample repo showing how a developer using the OpenSea Stream API might be able to connect a Discord webhook to send messages whenever new events occur for a collection.
 
 This particular codebase tracks all event types for 4 collections: Bored Ape Yacht Club, Aurory, Doodles, and Decentraland Wearables.
 
 ## Setup
+
 ```bash
 $ npm install
 ```
 
 Fill out the `.env` file with:
+
 - your OpenSea Stream API URL and API key
 - the Discord webhook ID and token for the Discord you would like to send messages to. TODO: Add instructions for how to get this.
 
 ## Running locally
+
 Assuming your set up above was completed successfully, run the following command to start the server:
+
 ```bash
 $ npm run dev
 ```
@@ -21,6 +26,7 @@ $ npm run dev
 To start a local dev server that tracks your local codebase and updates as you save changes. You can also run the server statically with `npm run build && npm run start`.
 
 ## Deploying
+
 ```bash
 $ heroku create
 Creating app... done, ⬢ pure-coast-44807
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { MessageEmbed, WebhookClient } from "discord.js";
-import * as express from 'express';
+import * as express from "express";
 import * as dotenv from "dotenv";
-import { OpenSeaStreamClient, EventType, LogLevel, Network } from '../local-dependencies/opensea-stream-js-sdk/src/index';
-import { WebSocket } from 'ws';
-import { getMessage, getTitle, getUrl } from './helpers';
+import {
+  OpenSeaStreamClient,
+  EventType,
+  LogLevel,
+  Network,
+} from "@opensea/stream-js";
+import { WebSocket } from "ws";
+import { getMessageEmbed, setListingMessageEmbed } from "./helpers";
 
 // Environment variables
 dotenv.config();
-const port = parseInt(process.env.PORT || '8080');
+const port = parseInt(process.env.PORT || "8080");
 const enabled = process.env.BOT_ENABLED || true;
 const webhookId = process.env.WEBHOOK_ID || "";
 const webhookToken = process.env.WEBHOOK_TOKEN || "";
@@ -18,71 +23,94 @@ const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
 
 // OpenSea Stream API Client
 const openseaClient = new OpenSeaStreamClient({
-    token: openseaApiToken,
-    network: Network.MAINNET,
-    connectOptions: {
-      transport: WebSocket
-    },
-	logLevel: LogLevel.DEBUG
-  });
-
+  token: openseaApiToken,
+  network: Network.MAINNET,
+  connectOptions: {
+    transport: WebSocket,
+  },
+  logLevel: LogLevel.DEBUG,
+});
 
 const app: express.Express = express();
 
 app.get("/", (req: express.Request, res: express.Response) => {
-	res.send("Hello OpenSea Webhooks!");
-	if (!enabled) {
-		res.send("Bot is disabled");
-		return;
-	}
-	const allEvents = Object.values(EventType);
+  res.send("Hello OpenSea Webhsooks!");
+  if (!enabled) {
+    res.send("Bot is disabled");
+    return;
+  }
+  const allEvents = [
+    EventType.ITEM_SOLD,
+    EventType.ITEM_LISTED,
+    EventType.ITEM_RECEIVED_BID,
+    EventType.ITEM_RECEIVED_OFFER,
+  ];
+  const allEventsOther = [
+    EventType.ITEM_SOLD,
+    EventType.ITEM_LISTED,
+    EventType.ITEM_RECEIVED_BID,
+  ];
 
-	openseaClient.onEvents('aurory', allEvents, (event: any) => {
-		console.log(event);
-		const embed = new MessageEmbed()
-			.setTitle(getTitle("Aurory", event.payload.item))
-			.setDescription(getMessage(event.payload.item, "Neon District", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.payload.item))
-			.setColor('#61C7D7');
-		webhookClient.send({
-			username: 'AuroryBots',
-			avatarURL: 'https://lh3.googleusercontent.com/Qzl6u460tNyzWFrlaLQIa2VMBc1HBX5X7IDfEYBbKV3q1p_BDVkqC-A7-DS5RA-IKagzD0m7J-LDpmr_XnY2ocsTcXGM01DXM7lqUg=s130',
-			embeds: [embed],
-		});
-	});
+  // openseaClient.onEvents('known-origin', allEvents, (event: BaseStreamMessage<unknown>) => {
+  // 	console.log(event);
+  // 	const embed = new MessageEmbed()
+  // 		.setTitle(getTitle("Aurory", event.payload.item))
+  // 		.setDescription(getMessage(event.payload.item, "Neon District", event.event_type, event.timestamp, event.payload))
+  // 		.setURL(getUrl(event.payload.item))
+  // 		.setColor('#61C7D7');
+  // 	webhookClient.send({
+  // 		username: 'AuroryBots',
+  // 		avatarURL: 'https://lh3.googleusercontent.com/Qzl6u460tNyzWFrlaLQIa2VMBc1HBX5X7IDfEYBbKV3q1p_BDVkqC-A7-DS5RA-IKagzD0m7J-LDpmr_XnY2ocsTcXGM01DXM7lqUg=s130',
+  // 		embeds: [embed],
+  // 	});
+  // });
+  openseaClient.onEvents("boredapeyachtclub", allEvents, (event: any) => {
+    console.log(event);
+    console.log(event.payload.item.metadata);
+    const embed = getMessageEmbed(event, "Bored Ape Yacht Club");
+    webhookClient.send({
+      username: "OpenSeaBot",
+      embeds: [embed],
+    });
+  });
 
-	openseaClient.onEvents('boredapeyachtclub', allEvents, (event: any) => {
-		console.log(event);
-		const embed = new MessageEmbed()
-			.setTitle(getTitle("Bored Ape Yacht Club", event.payload.item))
-			.setDescription(getMessage(event.payload.item, "Bored Ape Yacht Club", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.payload.item))
-			.setColor('#EF972C');
-		webhookClient.send({
-			username: 'BoredApeBot',
-			avatarURL: 'https://lh3.googleusercontent.com/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB=s130',
-			embeds: [embed],
-		});
-	});
+  openseaClient.onEvents(
+    "mutant-ape-yacht-club",
+    allEventsOther,
+    (event: any) => {
+      console.log(event);
+      console.log(event.payload.item.metadata);
+      const embed = getMessageEmbed(event, "Mutant Ape Yacht Club");
+      webhookClient.send({
+        username: "OpenSeaBot",
+        embeds: [embed],
+      });
+    }
+  );
 
-	openseaClient.onEvents('decentraland-polygon-wearables', allEvents, (event: any) => {
-		console.log(event);
-		const embed = new MessageEmbed()
-			.setTitle(getTitle("decentraland-polygon-wearables", event.payload.item))
-			.setDescription(getMessage(event.payload.item, "decentraland-polygon-wearables", event.event_type, event.timestamp, event.payload))
-			.setURL(getUrl(event.payload.item))
-			.setColor('#EF972C');
-		webhookClient.send({
-			username: 'Decentraland',
-			avatarURL: 'https://lh3.googleusercontent.com/Ju9CkWtV-1Okvf45wo8UctR-M9He2PjILP0oOvxE89AyiPPGtrR3gysu1Zgy0hjd2xKIgjJJtWIc0ybj4Vd7wv8t3pxDGHoJBzDB=s130',
-			embeds: [embed],
-		});
-	});
+  openseaClient.onEvents("otherdeed", allEventsOther, (event: any) => {
+    console.log(event);
+    console.log(event.payload.item.metadata);
+    const embed = getMessageEmbed(event, "Otherdeed");
+    webhookClient.send({
+      username: "OpenSeaBot",
+      embeds: [embed],
+    });
+  });
+
+  openseaClient.onEvents("bored-ape-kennel-club", allEvents, (event: any) => {
+    console.log(event);
+    console.log(event.payload.item.metadata);
+    const embed = getMessageEmbed(event, "Bored Ape Kennel Club");
+    webhookClient.send({
+      username: "OpenSeaBot",
+      embeds: [embed],
+    });
+  });
 });
 
-
-app.listen( port, () => {
-	console.log("Server is running...");
+app.listen(port, () => {
+  console.log("Server is running...");
 });
 
-app.use(express.static("public"))
+app.use(express.static("public"));
